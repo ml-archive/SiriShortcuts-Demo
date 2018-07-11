515b11c5f0a93dff8fd40f538f66e32d19254096
anho <anho@nodes.dk>
2018-07-11 14:40:19 +0200

    Initial Commit

---
+++ SiriSuggestionsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriSuggestionsDemo
+//
+//  Created by Andrei Hogea on 11/07/2018.
+//  Copyright © 2018 Andrei Hogea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
